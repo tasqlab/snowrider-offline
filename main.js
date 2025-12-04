24c20c8902c33f0bd5a806d9eae08ee77d6518b9
tasqlab <tchazq1n@gmail.com>
2025-12-04 16:53:11 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      nodeIntegration: false,
+    }
+  });
+
+  win.loadFile(path.join(__dirname, 'webgl-build/index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
